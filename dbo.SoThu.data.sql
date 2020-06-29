8c1666772605f2a9d1a6d021631e9b825f79a089
tachieule1101 <51347204+tachieule1101@users.noreply.github.com>
2020-06-29 10:25:38 +0700

    View

---
+++ dbo.SoThu.data.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[SoThu] ON
+INSERT INTO [dbo].[SoThu] ([Id], [name], [sothuthang], [kehoach]) VALUES (1, N'Đội 1     ', CAST(100000 AS Decimal(18, 0)), CAST(200000 AS Decimal(18, 0)))
+INSERT INTO [dbo].[SoThu] ([Id], [name], [sothuthang], [kehoach]) VALUES (2, N'Đội 2     ', CAST(300000 AS Decimal(18, 0)), CAST(350000 AS Decimal(18, 0)))
+SET IDENTITY_INSERT [dbo].[SoThu] OFF
